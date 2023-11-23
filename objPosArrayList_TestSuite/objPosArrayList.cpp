04ea76fc88434f195ddbfb8cc5b6983121aa7829
Matthew Chianchai <gobeilcm@mcmaster.ca>
2023-11-23 14:07:23 -0500

    thing7

--- objPosArrayList_TestSuite/objPosArrayList.cpp
+++ objPosArrayList_TestSuite/objPosArrayList.cpp
@@ -5,22 +5,32 @@
 
 objPosArrayList::objPosArrayList()
 {
-
+    aList = new objPos[ARRAY_MAX_CAP]; //200 elements on the heap
+    listsize = 0;                      // no valid element in the list upon startup
+    arrayCapacity = ARRAY_MAX_CAP;
 }
 
 objPosArrayList::~objPosArrayList()
 {
-
+    delete[] aList;
 }
 
 int objPosArrayList::getSize()
 {
-
+    return listsize;
 }
 
 void objPosArrayList::insertHead(objPos thisPos)
 {
+    //still need error check - what if listsize is at max cap
+    // check if listsize is equal to arraycap already. If yes, at cap, don't insert
+    for(int i = listsize;i>0;i--)
+    {
+        aList[i].setObjPos(aList[i-1]); // this will shufle all the elements towards the tial
+    }
 
+    aList[0].setObjPos(thisPos);
+    listsize++;
 }
 
 void objPosArrayList::insertTail(objPos thisPos)
@@ -40,15 +50,15 @@ void objPosArrayList::removeTail()
 
 void objPosArrayList::getHeadElement(objPos &returnPos)
 {
-
+    returnPos.setObjPos(aList[0]);
 }
 
 void objPosArrayList::getTailElement(objPos &returnPos)
 {
-
+    returnPos.setObjPos(aList[listsize-1]);
 }
 
 void objPosArrayList::getElement(objPos &returnPos, int index)
 {
-
+    
 }
